16038eae39ce849fc409fe17ae42e61fc53636a1
CelsoLuiz73 <oclinfo21@gmail.com>
2024-06-26 00:48:46 -0300

    primeiro commit

---
+++ src/Iphone.java
@@ -0,0 +1,46 @@
+public abstract class Iphone implements AparelhoTelefonico, NavegadorInternet, ReprodutorMusical{
+    @Override
+    public void ligar(String numero) {
+        //Fazer aqui implementacao do método.
+    }
+
+    @Override
+    public void atender(String numero) {
+        //Fazer aqui implementacao do método.
+    }
+
+    @Override
+    public void iniciarCorreioVoz(String mensagem, String destinatario) {
+        //Fazer aqui implementacao do método.
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        //Fazer aqui implementacao do método.
+    }
+
+    @Override
+    public void adicionarNovaAba(String url) {
+        //Fazer aqui implementacao do método.
+    }
+
+    @Override
+    public void atualizarPagina() {
+        //Fazer aqui implementacao do método.
+    }
+
+    @Override
+    public void tocar() {
+        //Fazer aqui implementacao do método.
+    }
+
+    @Override
+    public void pausar() {
+        //Fazer aqui implementacao do método.
+    }
+
+    @Override
+    public void selecionarMusica() {
+        //Fazer aqui implementacao do método.
+    }
+}
